70c5cd2deb75907096148b2af95f2895b5f97a0e
kacperjagiela <jagielakacper80@gmail.com>
2024-04-21 21:06:51 +0200

    feat: load file from path

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,22 @@
+use std::fs;
+
+fn load_file(path: &str) {
+    let file = fs::read(path);
+
+    match file {
+        Ok(data) => {
+            println!("File loaded: {:?}", data);
+        }
+        Err(_) => {
+            println!("Error loading file");
+        }
+    }
+}
+
 pub fn run() -> Result<(), ()> {
     println!("test");
+
+    load_file("rec.mov");
 
     Ok(())
 }
